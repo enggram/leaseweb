fff48c6a134d00c0ab10de7bb95a7b058c33e672
Ram <enggram@gmail.com>
2020-08-16 22:50:23 +0530

    Nginx Procfile and config

--- public/index.php
+++ public/index.php
@@ -11,6 +11,10 @@ if ($_SERVER['APP_DEBUG']) {
 
     Debug::enable();
 }
+header ("Access-Control-Allow-Origin: *");
+header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header ("Access-Control-Allow-Headers: *");
 
 $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
 $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
